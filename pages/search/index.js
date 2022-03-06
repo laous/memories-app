72dcac24b9371b93933e9ffc8e80b71a42503083
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-03-06 20:46:36 +0100

    fix error window undefined

--- pages/search/index.js
+++ pages/search/index.js
@@ -4,10 +4,8 @@ import MemoriesList from "../../components/MemoriesList";
 
 const index = () => {
   const query = "";
-  if (window !== undefined) {
-    const queryParams = new URLSearchParams(window.location.search);
-    query = queryParams.get("query");
-  }
+  const queryParams = new URLSearchParams(window.location.search);
+  query = queryParams.get("query");
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const list = useSelector((state) => state.memories.list);
