b880682ffcc0bd20edd2ee035553ca7a924e6274
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-03-06 20:53:55 +0100

    fix window err

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import MemoriesList from "../../components/MemoriesList";
 
 const index = () => {
-  const query = "";
-  const queryParams = new URLSearchParams(window.location.search);
-  query = queryParams.get("query");
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [query, setQuery] = useState("");
+  const queryParams = new URLSearchParams();
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    queryParams = new URLSearchParams(window.location.search);
+    setQuery(queryParams.get("query"));
+  }, []);
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const list = useSelector((state) => state.memories.list);
